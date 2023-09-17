30eee1c789fe9e659a4b808b805dddb5a2bc4c1d
bilgituncay <139182075+bilgituncay@users.noreply.github.com>
2023-09-17 19:16:54 +0200

    Update_Reg urls.py
    Added url for user registration.

--- Classifieds/Ads/urls.py
+++ Classifieds/Ads/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name='ads'
 urlpatterns = [
     path('', views.AdListView.as_view(), name='all'),
+    path('register/', views.register, name = 'register'),
     path('ad/<int:pk>', views.AdDetailView.as_view(), name='ad_detail'),
     path('ad/create', views.AdCreateView.as_view(success_url=reverse_lazy('ads:all')), name='ad_create'),
     path('ad/<int:pk>/update', views.AdUpdateView.as_view(success_url=reverse_lazy('ads:all')), name='ad_update'),
